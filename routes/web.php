924b6f5f0227936dff9f1397a56e72b63c4678d3
Mark Joseph Prieto <markprieto54@gmail.com>
2024-10-03 00:45:46 +0800

    10-03_Vid07-Models

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,25 @@
 <?php
 
-use Illuminate\Support\Arr;
+
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
+
 
 Route::get('/', function () {
     return view('Home');
 });
-Route::get('/jobs', function () {
+Route::get('/jobs', function ()  {
     return view('Jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$65,000'
-            ],
-            [
-                'id'=> 2,
-                'title' => 'Programmer',
-                'salary' => '$60,000'
-            ],
-            [
-                'id'=> 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$65,000'
-        ],
-        [
-            'id'=> 2,
-            'title' => 'Programmer',
-            'salary' => '$60,000'
-        ],
-        [
-            'id'=> 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ]
-    ];
+    $jobs =
 
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] === (int)$id);
+   $job = Job::find($id);
 
     dd($job);
 
