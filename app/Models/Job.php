54750c778436bff6be52b7d8ad7378f5a4ae71c4
Mark Joseph T. Prieto <markprieto54@gmail.com>
2024-10-12 02:24:31 +0800

    1011_Vid16_Forms_and_CSRF_Explained_

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,23 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job extends Model
-{
+class Job extends Model {
     use HasFactory;
 
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $guarded = [];
 
     public function employer()
     {
         return $this->belongsTo(Employer::class);
-    }
-
-    public function tags()
-    {
-
-        return $this->belongsToMany(Tag::class, 'job_tag', 'job_listing_id', 'tag_id');
-
     }
 }
