61028990f24aff49a8d05ad645185cb5a6659022
Mark Joseph T. Prieto <markprieto54@gmail.com>
2024-10-12 02:22:22 +0800

    1011_Vid16_Forms_and_CSRF_Explained_

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,42 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/', function () {
-    return view('Home');
+    return view('home');
 });
 
 Route::get('/jobs', function () {
-        $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('Jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
 
+Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
+    return view('jobs.show', ['job' => $job]);
+});
 
-    if (! $job) {
-        abort(404);
-    }
+Route::post('/jobs', function () {
+    // validation...
 
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
 
-    return view('job', ['job' => $job]);
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
-    return view('Contact');
+    return view('contact');
 });
